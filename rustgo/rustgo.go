f8f507d25c55f737150350e448906d146d19d207
Herbert Wolverson <herberticus@gmail.com>
2023-05-06 13:32:29 +0000

    Add timings to the Go program.

--- rustgo/rustgo.go
+++ rustgo/rustgo.go
@@ -7,8 +7,15 @@ package main
 import "C"
 
 import "fmt"
+import "time"
 
 func main() {
+	start := time.Now()
     fmt.Println("Hello from GoLang!")
+	duration := time.Since(start)
+	fmt.Println(duration)
+	start2 := time.Now()
 	C.hello(C.CString("from Rust!"))
+	duration2 := time.Since(start2)
+	fmt.Println(duration2)
 }
